0de767ee374435189f646418393726f1f20e6bb9
tianchengcheng-cn <tiancheng_@outlook.com>
2020-11-23 11:51:38 +0800

    master

--- MID/动态规划/5-最长回文字串.cpp
+++ MID/动态规划/5-最长回文字串.cpp
@@ -14,17 +14,22 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
+        // DP方法
         int n = s.size();
-        vector<char> revStr(n);
-        for (int i = 0; i < n; i++)
-        {
-            revStr[i] = s[n - i - 1];
-        }
-        int left = 0, right = 0;
-        int res = 0;
-        while (right < n)
-        {
+        vector< vector<int> > dp(n, vector<int>(n));
+        string ans;
 
+        for (int l = 0; l < n; ++l)
+        {
+            for (int i = 0; i + l < n; ++i)
+            {
+                int j = i + l;
+                if (l == 0) dp[i][j] = 1;
+                else if (l == 1) dp[i][j] == (s[i] == s[j]);
+                else dp[i][j] = (dp[i + 1][j - 1] && s[i] == s[j]);
+                if (dp[i][j] && l + 1 > ans.size()) ans = s.substr(i, l + 1);
+            }
         }
+        return ans;
     }
 };
