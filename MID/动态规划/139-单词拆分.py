b6fe0dac5aeb2e8f1fba6d0687dae8aeadad245b
tianchengcheng-cn <tiancheng_@outlook.com>
2021-01-13 14:43:42 +0800

    master

---
+++ MID/动态规划/139-单词拆分.py
@@ -0,0 +1,27 @@
+"""
+给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+说明：
+拆分时可以重复使用字典中的单词。
+你可以假设字典中没有重复的单词。
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false
+"""
+
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for word in wordDict:
+                if (i >= len(word) and s[i - len(word):i] == word):
+                    dp[i] = dp[i] or dp[i - len(word)]
+        return dp[-1]
+
+
