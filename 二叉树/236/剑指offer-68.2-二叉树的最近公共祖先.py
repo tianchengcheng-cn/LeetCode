bd76542564466ceca0565c5705f2b80820092c23
tianchengcheng-cn <tiancheng_@outlook.com>
2021-01-18 17:17:56 +0800

    master

---
+++ 二叉树/236/剑指offer-68.2-二叉树的最近公共祖先.py
@@ -0,0 +1,26 @@
+
+"""
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出: 3
+解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q: return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left: return right
+        if not right: return left
+        return root
