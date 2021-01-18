129a277b000c32c8eb4e98097b0b96e8792453a6
tianchengcheng-cn <tiancheng_@outlook.com>
2021-01-18 17:39:29 +0800

    master

---
+++ 二叉树/235&剑指off68.1-二叉搜索树的最近公共祖先.py
@@ -0,0 +1,27 @@
+"""
+给定一个【二叉搜索树】, 找到该树中两个指定节点的最近公共祖先。
+
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+输出: 6 
+解释: 节点 2 和节点 8 的最近公共祖先是 6。
+
+时空复杂度：O(n)，O(1)，没有使用到递归的栈，因此空间复杂度为常数大小,即，O(1)
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                break
+        return root
